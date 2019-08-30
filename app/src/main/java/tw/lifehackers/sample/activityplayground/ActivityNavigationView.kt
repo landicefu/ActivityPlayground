185781895c7e80be0694d4bbe3d56b1a2358b690
Landice Fu <landicefu@gmail.com>
2019-08-30 18:24:06 +0800

    Add all flags

--- app/src/main/java/tw/lifehackers/sample/activityplayground/ActivityNavigationView.kt
+++ app/src/main/java/tw/lifehackers/sample/activityplayground/ActivityNavigationView.kt
@@ -10,7 +10,22 @@ interface ActivityNavigationView {
             "clear top" to Intent.FLAG_ACTIVITY_CLEAR_TOP,
             "clear task" to Intent.FLAG_ACTIVITY_CLEAR_TASK,
             "new document" to Intent.FLAG_ACTIVITY_NEW_DOCUMENT,
-            "new task" to Intent.FLAG_ACTIVITY_NEW_TASK
+            "new task" to Intent.FLAG_ACTIVITY_NEW_TASK,
+            "no history" to Intent.FLAG_ACTIVITY_NO_HISTORY,
+            "multiple task" to Intent.FLAG_ACTIVITY_MULTIPLE_TASK,
+            "forward result" to Intent.FLAG_ACTIVITY_FORWARD_RESULT,
+            "previous is top" to Intent.FLAG_ACTIVITY_PREVIOUS_IS_TOP,
+            "exclude from recents" to Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS,
+            "brought to front" to Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT,
+            "reset task if needed" to Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED,
+            "launched from history" to Intent.FLAG_ACTIVITY_LAUNCHED_FROM_HISTORY,
+            "no user action" to Intent.FLAG_ACTIVITY_NO_USER_ACTION,
+            "reorder to front" to Intent.FLAG_ACTIVITY_REORDER_TO_FRONT,
+            "no animation" to Intent.FLAG_ACTIVITY_NO_ANIMATION,
+            "task on home" to Intent.FLAG_ACTIVITY_TASK_ON_HOME,
+            "retain in recents" to Intent.FLAG_ACTIVITY_RETAIN_IN_RECENTS,
+            "launch adjacent" to Intent.FLAG_ACTIVITY_LAUNCH_ADJACENT,
+            "match external" to Intent.FLAG_ACTIVITY_MATCH_EXTERNAL
         )
     }
 
